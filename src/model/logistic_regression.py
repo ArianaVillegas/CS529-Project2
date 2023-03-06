fffaa2156c4bf135b60d4f5051f17cd9aa1d5b84
Enrique720 <enrique.sobrados@utec.edu.pe>
2023-03-06 11:59:37 -0700

    prediction() and gradient_descent() update

--- src/model/logistic_regression.py
+++ src/model/logistic_regression.py
@@ -9,37 +9,39 @@ class LogisticRegression:
     Parameters
     ----------
     lr : learning rate for gradient descent.
+    reg : Regularization
     """
 
-    def __init__(self, lr = 0.01):
+    def __init__(self, lr = 0.01, reg=0.5):
         self.lr = lr
+        self.reg = reg
         return
     
-    def gradient_descent(self, y, ypred):
-        self.w = self.w + self.lr * (y - ypred)  - self.lr*self.w
+    def gradient_descent(self, x, y, ypred):
+        self.w = self.w + self.lr * (np.matmul(x, np.transpose(y - ypred))) - self.lr*self.reg*self.w
         return
     
 
-    def prediction(self, x, y):
+    def prediction(self, x):
         # previous calculations
-        calculation =  np.exp(self.w0 + np.diagonal(np.dot(x, np.transpose(self.w0))))
+        #calculation =  np.exp(self.w0 + np.dot(self.w, np.transpose(x)))
+        first_factor = np.exp(self.w0 + np.matmul(x, np.transpose(self.w)))
+        second_factor = (np.sum(first_factor,axis=1)).reshape(-1,1)        
+        probs = first_factor/(1+second_factor)
+        probs = np.concatenate(probs[:,:-1], 1/(1+second_factor)) # concatenate all but the last class.
+        # Calculate probabilities for all classes
+        #probs = calculation/(1+np.sum(calculation))
+        #probs = np.append(probs, 1/(1+np.sum(calculation)))
+        return probs
 
-        # Calculate probability of first class
-        probs = calculation / 1 + np.sum(calculation)
-        last_prob = 1 / 1 + np.sum(calculation)
-
-        return np.append(probs, np.array(last_prob))
-
-    def train(self, x, y):
+    def fit(self, x, y, iterations=1000):
         # Initialize weights matrix
-        self.w0 = np.random.rand(len(np.unique(y)))
-        self.w = np.random.rand(len(np.unique(y))-1,len(x[0]))
-        # Predictions
-        
-        # Gradient Descent 
-        self.gradient_descent()
-
-        # 
-    
-
+        n_rows = np.unique(y).T.shape
+        self.w0 = np.random.rand(*n_rows) 
+        self.w = np.random.rand(*(n_rows), *(x[0].T.shape))
+        for i in range(iterations):
+            # Predictions 
+            y_pred = self.prediction(x)
+            # Gradient Descent 
+            self.gradient_descent(x,y,y_pred)
         return
