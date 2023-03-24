4fa9b3b3d328faa1fa5e41fd3739328b61875573
Enrique720 <enrique.sobrados@utec.edu.pe>
2023-03-24 16:36:08 -0600

    changed to sparse matrix oprations

--- main_logistic_regression.py
+++ main_logistic_regression.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix, save_npz, load_npz
 import torch
+import time
 
 from sklearn.model_selection import train_test_split
 
@@ -37,59 +38,68 @@ if __name__ == '__main__':
     train_array = read_large_df(args.train_file)
     train_x, train_y = train_array[:,1:-1], train_array[:,-1]
 
+
+    np.random.seed(3) #set numpy random seed    
+
     # Converting to one hot ecoding
     train_y = train_y.toarray().flatten().astype(int)
     train_y = torch.from_numpy(train_y)
     train_y = torch.nn.functional.one_hot(train_y)
     train_y = csr_matrix(train_y)[:, 1:]
     
-
-    '''
-    scores = cross_validation_split(train_x, train_y, LogisticRegression())
-    print(f'Accuracy: {np.round(np.mean(scores)*100, 2)}%')
-
-    b_acc = []
-    b_std = []
-    b_val = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1]
-    for b in b_val:
-        scores = cross_validation_split(train_x, train_y, NaiveBayes(beta=b), n_splits=5)
-        acc = np.mean(scores * 100)
-        std = np.std(scores * 100)
-        b_acc.append(acc)
-        b_std.append(std)
-        plt.annotate(str(np.round(acc, 2)), xy=(b*1.25,acc-0.4), fontsize=14)
-    plt.plot(np.array(b_val), np.array(b_acc))
-    plt.scatter(np.array(b_val), np.array(b_acc))
-    plt.fill_between(np.array(b_val), np.array(b_acc) - np.array(b_std), np.array(b_acc) + np.array(b_std), alpha=0.2)
-    plt.xscale('log')
-    plt.xlim(right=8)
-    # plt.ylim(bottom=83.5)
-    plt.xlabel('Beta', fontsize=18)
-    plt.ylabel('Accuracy', fontsize=18)
-    plt.xticks(fontsize=18)
-    plt.yticks(fontsize=18)
-    plt.tight_layout()
-    plt.savefig('acc_naive_bayes.png')
-    '''
-    
     train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size=0.3, random_state=42)
     train_y = train_y.toarray()
     val_y = val_y.toarray()
 
-
+    
+    #scores = cross_validation_split(train_x, train_y, LogisticRegression())
+    #print(f'Accuracy: {np.round(np.mean(scores)*100, 2)}%')
+    
+    b_acc = []
+    b_std = []
+    lr_val = [0.001, 0.01, 0.1, 1]
+    for lr in lr_val:
+        scores = cross_validation_split(train_x, train_y, LogisticRegression(lr=lr), n_splits=4)
+        acc = np.mean(scores * 100)
+        std = np.std(scores * 100)
+        b_acc.append(acc)
+        b_std.append(std)
+        plt.annotate(str(np.round(acc, 2)), xy=(lr*1.25,acc-0.4), fontsize=14)
+    plt.plot(np.array(lr_val), np.array(b_acc))
+    plt.scatter(np.array(lr_val), np.array(b_acc))
+    plt.fill_between(np.array(lr_val), np.array(b_acc) - np.array(b_std), np.array(b_acc) + np.array(b_std), alpha=0.2)
+    plt.xscale('log')
+    plt.xlim(right=8)
+    # plt.ylim(bottom=83.5)
+    plt.xlabel('lr', fontsize=18)
+    plt.ylabel('Accuracy', fontsize=18)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.tight_layout()
+    plt.savefig('acc_logistic_regression.png')
+    
+    
     ''' Training the model '''
+    
+    
+    '''
     model = LogisticRegression()
-    model.train(train_x, train_y)
+    num_iterations = 100
+    start_time=time.time()
+    model.train(train_x, train_y, iterations=num_iterations)
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f"Total training time taken for {num_iterations} iterations: {total_time:.6f} seconds")
+
     train_pred = model.eval(train_x)
     val_pred = model.eval(val_x)
     
-
-    ''' Printing the accuracy '''
+     Printing the accuracy 
     print("Train accuracy accuracy: " + str(accuracy(train_pred,train_y)*100))
     print("Val accuracy: " + str(accuracy(val_pred, val_y)*100))
     
     #plot_confussion_matrix(val_y, val_pred, labels, 'cm_logistric_regression.png')
-    
+    '''
     '''
     sort_idx = model.rank_words(topk=20)
     words = vocabulary[sort_idx]
--- src/model/logistic_regression.py
+++ src/model/logistic_regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import scipy.sparse as sp
 
 def normalize_array(arr, epsilon=1):
     """
@@ -28,45 +28,56 @@ class LogisticRegression:
         return
     
     def gradient_descent(self, x, y, ypred):
-        grad = (np.matmul(np.transpose(y-ypred),x)) 
+        rest = y - ypred
+        grad = rest.transpose() @ x
         self.w = self.w +  self.lr*grad - self.lr*self.reg*self.w
         return
     
 
     def prediction(self, x):
-        # previous calculations
-        first_factor = np.exp(x @ self.w.T)
+        
+        first_factor = x @ self.w.T
+        first_factor = sp.csr_matrix(np.exp(first_factor.toarray()))
         second_factor = (np.sum(first_factor,axis=1)).reshape(-1,1)
         probs = first_factor/(1+second_factor)
         last_prob = 1/(1+second_factor)
-        probs = np.concatenate((probs[:,:-1], last_prob), axis=1) # concatenate all but the last class
+        probs = np.concatenate((probs[:,:-1], last_prob), axis=1)
         return probs
 
     def eval(self,x):
         x = x.toarray()
         x = normalize_array(x)
         x = np.insert(x,0,np.ones(len(x)), axis=1)
+        x = sp.csr_matrix(x)
         probs = self.prediction(x)
+        probs = np.array(probs)
         max_index = np.argmax(probs,axis=1)
         one_hot = np.zeros_like(probs)
         one_hot[np.arange(probs.shape[0]),max_index] = 1
         return one_hot
 
 
-    def train(self, x, y, iterations=30):
+    def train(self, x, y, iterations=100):
         x = x.toarray()
         x = normalize_array(x)
         # Initialize weights matrix
-        n_rows = len(y[0])
+        n_rows = len(y[0])  
         self.n_clasess= n_rows
         n_columns = (x[0].T.shape)[0]
         self.w = np.random.rand(n_rows,n_columns+1)
+        self.w = sp.csr_matrix(self.w)
         x = np.insert(x,0,np.ones(len(y)), axis=1)
-        print(self.w[:,0])
+
+        # Convert to sparse matrix
+        x = sp.csr_matrix(x)
+        y = sp.csr_matrix(y)
+        print(self.w[:,1])
+        
         for i in range(iterations):
             # Predictions 
             y_pred = self.prediction(x)
+            y_pred = sp.csr_matrix(y_pred)
             # Gradient Descent 
             self.gradient_descent(x,y,y_pred)
-        print(self.w[:,0])
-        return
+        print(self.w[:,1])
+        return  
